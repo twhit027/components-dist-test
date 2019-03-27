f7bef3adadeaea431b3c61df3bcc8a88feeb21c7
twhit027 <trewhite@gannett.com>
2019-03-27 12:00:25 -0400

    init

---
+++ esm/es2017/build/jl4qfmhb.entry.js
@@ -0,0 +1,37 @@
+import { h } from '../mycomponent.core.js';
+
+function format(first, middle, last) {
+    return ((first || '') +
+        (middle ? ` ${middle}` : '') +
+        (last ? ` ${last}` : ''));
+}
+
+class MyComponent {
+    getText() {
+        return format(this.first, this.middle, this.last);
+    }
+    render() {
+        return (h("div", { class: "blue" },
+            "Hello, World! I'm ",
+            this.getText()));
+    }
+    static get is() { return "my-component"; }
+    static get encapsulation() { return "shadow"; }
+    static get properties() { return {
+        "first": {
+            "type": String,
+            "attr": "first"
+        },
+        "last": {
+            "type": String,
+            "attr": "last"
+        },
+        "middle": {
+            "type": String,
+            "attr": "middle"
+        }
+    }; }
+    static get style() { return ":host{text-rendering:optimizeLegibility;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;color:#222;font-family:Unify Sans,Helvetica,Arial,sans-serif;font-size:calc(18px + (22 - 18) * ((100vw - 320px) / (1200 - 320)));font-weight:600;line-height:1.15}\@font-face{font-family:Unify Sans;src:url(/src/assets/fonts/UnifySans_W_Rg.woff2) format(\"woff2\");font-style:normal;font-weight:400}\@font-face{font-family:Unify Sans;src:url(/src/assets/fonts/UnifySans_W_Sbd.woff2) format(\"woff2\");font-style:normal;font-weight:600}\@font-face{font-family:Unify Sans;src:url(/src/assets/fonts/UnifySans_W_Bd.woff2) format(\"woff2\");font-style:normal;font-weight:900}\@font-face{font-family:\"Unify Serif\";src:url(/src/assets/fonts/UnifySans_W_Rg.woff2) format(\"woff2\");font-style:normal;font-weight:400}\@font-face{font-family:\"Unify Serif\";src:url(/src/assets/fonts/UnifySans_W_Sbd.woff2) format(\"woff2\");font-style:normal;font-weight:600}\@font-face{font-family:\"Unify Serif\";src:url(/src/assets/fonts/UnifySans_W_Bd.woff2) format(\"woff2\");font-style:normal;font-weight:900}"; }
+}
+
+export { MyComponent };
